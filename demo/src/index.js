4f97d153f30c4e0ad70e63a1ab95b100eac86f97
ninemilli.song <ninemilli_song@163.com>
2018-10-03 20:25:25 +0800

    test

--- demo/src/index.js
+++ demo/src/index.js
@@ -6,7 +6,7 @@ import Example from '../../src'
 class Demo extends Component {
   render() {
     return <div>
-      <h1>aladdin-voucher Demo</h1>
+      <h1>aladdin-voucher Demo!</h1>
       <Example/>
     </div>
   }
